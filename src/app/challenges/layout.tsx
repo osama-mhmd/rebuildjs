eb6a3b7ef70597c84581fc08c69489777ad42fbc
os-mhmd <docosmo010@gmail.com>
2024-08-01 19:37:30 +0300

    fix/issue-in-layout

--- src/app/challenges/layout.tsx
+++ src/app/challenges/layout.tsx
@@ -9,9 +9,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html>
-      <body>{children}</body>
-    </html>
-  );
+  return children
 }
