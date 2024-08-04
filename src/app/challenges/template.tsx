9e1ebf720a3ff129c8bb3015572897c9a25f82cd
os-mhmd <docosmo010@gmail.com>
2024-08-04 18:57:35 +0300

    refactor/template-refactor

--- src/app/challenges/template.tsx
+++ src/app/challenges/template.tsx
@@ -1,12 +1,3 @@
-"use client";
-
 import Template from "@/app/template";
-import { ReactNode } from "react";
 
-export default function ChallengesTemplate({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <Template>{children}</Template>;
-}
+export default Template;
