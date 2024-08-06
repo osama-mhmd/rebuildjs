5ca04c5b250e733a8316f181258b5c453ddd6489
os-mhmd <docosmo010@gmail.com>
2024-08-06 22:53:02 +0300

    feat/ux-enhanced-by-adding-nextjs-toploader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader />
         {children}
         <Footer />
       </body>
