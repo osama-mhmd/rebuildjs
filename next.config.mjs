34552c1d35b68847005d0e5f983d24e2efd93263
os-mhmd <docosmo010@gmail.com>
2024-08-08 18:56:11 +0300

    fix/shiki-add-to-experimental-packages-as-it-uses-esmodules

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ["shiki"],
+  },
+};
 
 export default nextConfig;
