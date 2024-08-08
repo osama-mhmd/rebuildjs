9612c0542a99b08742b6660792770832db662649
os-mhmd <docosmo010@gmail.com>
2024-08-08 20:28:04 +0300

    fix/shiki-added-to-transpile-packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["shiki"],
   experimental: {
-    optimizePackageImports: ["shiki"],
+    // optimizePackageImports: ["shiki"],
+    // TypeError: Cannot read properties of undefined (reading 'transformers')
+    // shiki added to experimental packages as it uses ES modules => nextjs error = ERR_REQUIRE_ESM,
+    // but then added to transpiledPackages
   },
 };
 
